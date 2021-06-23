37a9482c6aa9d0bc2af4cfe0efe44f967fe5a1dc
zwkj099 <zwkj099@163.com>
2021-06-23 22:52:22 +0800

    练习1

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,4 +4,15 @@
  * @date 2021/6/19 10:42
  */
 public class Test {
+    public static void main(String [] args){
+        Test t= new Test();
+        t.age(33);
+    }
+    public  void age(int age){
+    if (age <0){
+        System.out.println("不可能");
+    }else {
+        System.out.println("你太老了");
+    }
+    }
 }
